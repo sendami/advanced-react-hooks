154c1a43d16e58d4b43a0b2c505b0b0ac57267b5
sendami luque <sendami.luque@thoughtworks.com>
2024-08-20 15:39:31 +0200

    Exercise 3 - ec 1

--- src/exercise/03.js
+++ src/exercise/03.js
@@ -3,7 +3,7 @@
 
 import * as React from 'react'
 
-const CountContext = React.createContext()
+const CountContext = React.createContext([0, () => {}])
 
 function CountProvider(props) {
   const [count, setCount] = React.useState(0)
@@ -11,13 +11,21 @@ function CountProvider(props) {
   return <CountContext.Provider value={value} {...props} />
 }
 
+function useCount() {
+  const context = React.useContext(CountContext)
+  if (!context) {
+    throw new Error('useCount must be rendered within the CountProvider')
+  }
+  return context;
+}
+
 function CountDisplay() {
-  const [count] = React.useContext(CountContext)
+  const [count] = useCount()
   return <div>{`The current count is ${count}`}</div>
 }
 
 function Counter() {
-  const [, setCount] = React.useContext(CountContext)
+  const [, setCount] = useCount()
   const increment = () => setCount(c => c + 1)
   return <button onClick={increment}>Increment count</button>
 }
