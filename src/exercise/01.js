2471de947284b98d66fc71da3dc08f1259bb0e1e
sendami luque <sendami.luque@thoughtworks.com>
2024-08-20 10:39:31 +0200

    Exercise 1 - ec 2

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -3,12 +3,17 @@
 
 import * as React from 'react'
 
-const countReducer = (count, change) => count + change
+const countReducer = (state, action) => ({...state, ...action})
 
 function Counter({initialCount = 0, step = 1}) {
-  const [count, changeCount] = React.useReducer(countReducer, initialCount)
+  const [state, setState] = React.useReducer(
+    countReducer, {
+      count: initialCount,
+    },
+  )
+  const {count} = state
+  const increment = () => setState({count: count + step})
 
-  const increment = () => changeCount(step)
   return <button onClick={increment}>{count}</button>
 }
 
